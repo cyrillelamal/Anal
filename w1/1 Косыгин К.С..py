c0a1407a9fb2a72defa137a065c39843827938a3
Cyrille <cyrillekossyguine@gmail.com>
2019-09-25 16:46:14 +0300

    lab1

---
+++ w1/1 Косыгин К.С..py
@@ -0,0 +1,33 @@
+import math
+
+
+VALS = [14.85, 14.80, 14.79, 14.84, 14.81]
+x0 = 14.80
+ta = 2.570
+n = len(VALS)
+
+PRECISION = 5
+
+
+# Среднее значение
+x_ = round(sum(VALS) / n, PRECISION)
+print('Среднее значение: ', x_)
+
+# Дисперсия
+Ds2 = round(math.sqrt(
+    (sum([(x_ - xi)**2 for xi in VALS]))
+    / (n*(n-1))
+), PRECISION)
+print('Дисперсия (среднеквадратичная ошибка): ', Ds2)
+
+# Стандартное отклонение
+DS = round(math.sqrt(Ds2**2), PRECISION)
+print('Стандартное отклонение: ', DS)
+
+# Абсолютная погрешность (Доверительный интервал)
+Dx = round(ta * DS, PRECISION)
+print('Абсолютная погрешность: x={0}±{1}'.format(x_, Dx))
+
+# Относительная погрешность
+dx = round(Dx / x_ * 100, 2)
+print('Относительная погрешность: {}%'.format(dx))

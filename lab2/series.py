564ed6d73b0afeaeafa4eb6d3a5fab6745105e25
Cyrille <cyrillekossyguine@gmail.com>
2019-10-13 15:48:48 +0300

    refactored

--- lab2/series.py
+++ lab2/series.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def sorted_by_keys(dict_: dict) -> dict:
-    """Return sorted by keys copy of the passed dict"""
+    """Return copy of the passed dict sorted by its keys"""
     return {k: dict_[k] for k in sorted(dict_.keys())}
 
 
@@ -13,10 +13,27 @@ class VariationSeries:
     PRECISION = 1  # {number},{PRECISION}
 
     def __init__(self, values):
-        self._values = values
-        self.n = len(values)
-        self.max = max(self._values)
-        self.min = min(self._values)
+        """
+        :param values: all the values that make up the series
+        """
+        self._vs = None  # Вариационный ряд
+        self._values = sorted(values)
+        self._n = len(values)
+        self._x_max = max(self._values)
+        self._x_min = min(self._values)
+
+        self._var_frequencies = None  # Частота варианты - m(i)
+        self._rel_frequencies = None  # Относительная частота (частость) - w(i)
+        self._acc_frequencies = None  # Накопленная частота - m(x)
+        self._acc_rel_frequencies = None  # Накопленная частость w(x)
+
+    def _gen_acc_frequencies(self):
+        """Generator"""
+        accumulated_sum = 0
+        for m_i in self._vs.values():
+            yield accumulated_sum
+            accumulated_sum += m_i
+        yield accumulated_sum
 
     @staticmethod
     def show():
@@ -66,21 +83,38 @@ class VariationSeries:
 class DiscreteVS(VariationSeries):
     """Discrete variation series"""
     def __init__(self, values):
-        # User's input is ready variable series
+        # User's input is a ready variable series: build the list of values
         if isinstance(values, dict):
-            self.vs = sorted_by_keys(values)
-            selection = []
-            for k, v in values.items():
-                selection.extend([k]*v)
-            values = selection  # self._values (all values)
-        # User's input is a list of values
+            vs = sorted_by_keys(values)
+            values = []
+            for k, v in vs.items():
+                values += [k] * v
+        # User's input is a list of values: build the variable series
         else:
-            self.vs = {}
+            vs = {}
             for val in sorted(values):
-                self.vs[val] = self.vs.get(val, 0) + 1
-        # Else errors are thrown
-        # Save the list of values
+                vs[val] = vs.get(val, 0) + 1
+
         super(DiscreteVS, self).__init__(values)
+        self._vs = vs
+
+        self._variants = list(self._vs.keys())
+        self._var_frequencies = list(self._vs.values())
+        self._rel_frequencies = list(map(
+            lambda m_i: m_i / self._n, self._var_frequencies
+        ))
+        self._acc_frequencies = list(self._gen_acc_frequencies())
+        self._acc_rel_frequencies = list(map(
+            lambda m_x: m_x / self._n, self._acc_frequencies
+        ))
+
+        self._x = self.__find_next_x()  # Следующий x
+
+    def __find_next_x(self):
+        keys = list(self._vs.keys())
+        biggest = keys[-1]
+        diff = biggest - keys[-2]
+        return biggest + diff
 
     def draw_polygon(self, nrows, ncols, index):
         xs = self.get_polygon_xs()
@@ -92,57 +126,89 @@ class DiscreteVS(VariationSeries):
         plt.ylabel('Частоты')
 
     def get_polygon_xs(self):
-        return list(self.vs.keys())  # x(i)
+        return self._variants
 
     def get_polygon_ys(self):
-        return list(self.vs.values())  # m(i)  (Frequencies)
+        return self._var_frequencies
 
     def get_cumulate_xs(self):  # x(i)
-        keys = list(self.vs.keys())
-        last_key = keys[-1]
-        last_key += 1
-        keys += [last_key]
-        return keys
+        return self._variants + [self._x]
 
     def get_cumulate_ys(self):  # m(x(i))
-        def y_gen():
-            accumulated_sum = 0
-            for m_i in self.vs.values():
-                yield accumulated_sum
-                accumulated_sum += m_i
-            yield accumulated_sum
-        return list(y_gen())
+        return self._acc_frequencies
 
     def get_empiric_dist_xs(self):  # x(i)
         return self.get_cumulate_xs()
 
-    def get_empiric_dist_ys(self):  # w(x)
-        return [m_x / self.n for m_x in self.get_cumulate_ys()]
+    def get_empiric_dist_ys(self):  # w(x(i))
+        return self._acc_rel_frequencies
 
 
 class ContinuousVS(VariationSeries):
     """Continuous interval"""
     def __init__(self, values):
-        super(ContinuousVS, self).__init__(values)
-        self.k = math.ceil(1 + 1.4 * math.log(self.n))
-        self.delta = (self.max - self.min) / self.k
+        # User's input is a dict with intervals
+        if isinstance(values, dict):
+            # Complete redefinition
+            self._vs = values
+            self._intervals = list(values.keys())
 
-        p = VariationSeries.PRECISION  # Precision alias
-        # INTERVALS
-        self.intervals = [
-            (round(self.min + bias, p), round(self.min + bias + self.delta, p))
-            for bias in (i * self.delta for i in range(self.k))
-        ]
+            values = list(values.keys())  # tuples
+            self._n = sum(self._vs.values())
+            self._x_max = values[-1][-1]
+            self._x_min = values[0][0]
 
-        # VARIABLE SERIES
+            self._k = math.ceil(1 + 1.4 * math.log(self._n))  # Количество интервалов
+            self._delta = (self._x_max - self._x_min) / self._k  # Длина интервала
+
+        # User's input is a list of values
+        else:
+            super(ContinuousVS, self).__init__(values)
+
+            self._k = math.ceil(1 + 1.4 * math.log(self._n))  # Количество интервалов
+            self._delta = (self._x_max - self._x_min) / self._k  # Длина интервала
+
+            self._intervals = self.__make_intervals()
+            self._vs = self.__make_vs()
+
+        # self._variants = self._intervals  # Here intervals
+        self._var_frequencies = list(self._vs.values())
+        self._rel_frequencies = list(map(
+            lambda m_i: m_i / self._n, self._var_frequencies
+        ))
+        self._acc_frequencies = list(self._gen_acc_frequencies())
+        self._acc_rel_frequencies = list(map(
+            lambda m_x: m_x / self._n, self._acc_frequencies
+        ))
+
+    def __make_intervals(self) -> list:
+        """Return list of tuples intervals"""
+        p = VariationSeries.PRECISION  # Precision
+        x_start = self._x_min  # - self._delta / 2  # x(нач)
+        x_end = self._x_max + self._delta / 2  # Catch the biggest value
+        
+        intervals = []
+        for i in range(self._k):  # Number of interval
+            bias = i * self._delta
+            left = round(x_start + bias, p)
+            right = round(x_start + self._delta + bias, p)
+            intervals += [(left, right)]
+        last_interval = intervals[-1]
+        new_last_interval = (last_interval[0], x_end)
+        intervals[-1] = new_last_interval
+        return intervals
+
+    def __make_vs(self) -> dict:
+        """Return dict with variable series"""
         vs = {}  # Frequencies
         for val in sorted(self._values):
             # Choose the correct interval
-            for interval in self.intervals:
+            for interval in self._intervals:
                 left, right = interval
-                if left <= val < right or val == self.max:
+                if left <= val < right or val == self._x_max:
                     vs[interval] = vs.get(interval, 0) + 1
-        self.vs = vs
+                    break
+        return vs
 
     def draw_hist(self):
         xs = self.get_hist_xs()
@@ -155,33 +221,22 @@ class ContinuousVS(VariationSeries):
         fig.set_figheight(6)
 
     def get_hist_xs(self):
-        x_min = self.intervals[0][0]
-        center = self.delta / 2
-        xs = [
-            x_min + center + self.delta * i
-            for i in range(self.k)
-        ]
+        bias = self._delta / 2
+        xs = [interval[0] + bias for interval in self._vs.keys()]
         return xs
 
     def get_hist_ys(self):
-        return list(self.vs.values())
+        return self._var_frequencies
 
     def get_cumulate_xs(self):  # a(i)
-        xs = [interval[0] for interval in self.intervals] \
-             + [self.intervals[-1][1]]
+        xs = [interval[0] for interval in self._vs.keys()] + [self._intervals[-1][1]]
         return xs
 
     def get_cumulate_ys(self):  # m(a(i))
-        def y_gen():
-            accumulated_sum = 0
-            for a_i in self.vs.values():
-                yield accumulated_sum
-                accumulated_sum += a_i
-            yield accumulated_sum
-        return list(y_gen())
+        return self._acc_frequencies
 
     def get_empiric_dist_xs(self):
         return self.get_cumulate_xs()
 
     def get_empiric_dist_ys(self):
-        return [m_x / self.n for m_x in self.get_cumulate_ys()]
+        return self._acc_rel_frequencies

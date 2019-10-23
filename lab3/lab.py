1eee339c8348df5c5016230af1087e5ef76dcd52
Cyrille <cyrillekossyguine@gmail.com>
2019-10-23 10:43:12 +0300

    w3 drafts

--- lab3/lab.py
+++ lab3/lab.py
@@ -1,4 +1,5 @@
 import math
+from functools import reduce
 
 from lab2.series import ContinuousVS
 
@@ -6,7 +7,7 @@ from lab2.series import ContinuousVS
 PRECISION = 2
 
 
-values = [
+VALUES1 = [
         103.4, 115.2, 127, 131, 114, 114.1, 119.6, 125.5, 116.9, 118.1, 123.5,
         113.5, 112.3, 123, 125, 129.9, 99.2, 111, 122, 134, 107.1, 117, 117.5,
         118.5, 124, 127.8, 108, 119.5, 123, 126.1, 100.1, 120.2, 122.2, 124.8,
@@ -20,7 +21,7 @@ values = [
 
 
 def task1():
-    v = ContinuousVS(values)
+    v = ContinuousVS(VALUES1)
 
     def prod_gen():
         for interval, n_i in v.vs.items():
@@ -33,14 +34,26 @@ def task1():
 
 
 def task2():
+    # TODO: медиана - по кумулянте, не по гистограме
     ContinuousVS.PRECISION = 2
-    v = ContinuousVS(values)
+    v = ContinuousVS(VALUES1)
     v.draw_hist(1, 1, 1)
     ContinuousVS.show()
-    if v.n % 2 == 0:
-        # take the middle
-        idx = v.n // 2
-    else:
-        idx = v.n / 2
-    mediana = v.values[idx]
-    print(f'Медиана: {mediana}')
+
+    idx = v.n // 2 if v.n % 2 == 0 else v.n / 2
+    median = v.values[idx]
+    print(f'Медиана: {median}')
+
+
+def task3():
+    v = ContinuousVS(VALUES1)
+    idx = v.n // 2 if v.n % 2 == 0 else v.n / 2
+    median = v.values[idx]
+    print(f'Расчитанная медиана: {median}')
+
+    ys = v.get_cumulate_ys()
+    xs = v.get_cumulate_xs()
+    y_mid = (max(ys) + min(ys)) / 2
+    print(xs, ys)
+    # v.draw_cumulate(1, 1, 1)
+    # ContinuousVS.show()
--- w3.py
+++ w3.py
@@ -2,4 +2,5 @@ from lab3 import lab as l
 
 
 l.task1()
-l.task2()
+# l.task2()
+l.task3()
